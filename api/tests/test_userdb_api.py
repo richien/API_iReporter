95aebb6e7ee78e37ccb60b1d1bffec7550192ef3
Geoffrey Willis <benwill2014@gmail.com>
2019-01-22 09:47:18 +0300

    Add apis for querying incident and user table
    - Create method to get and create a user

---
+++ api/tests/test_userdb_api.py
@@ -0,0 +1,42 @@
+import unittest
+from api.models.database import userdb_api
+from api.models.user_model import User
+from api import app
+
+
+class TestUserDbApi(unittest.TestCase):
+
+    def setUp(self):
+        self.user_data = {
+            "firstname": "Henry",
+            "lastname": "Jones",
+            "othernames": "",
+            "email": "email@email.com",
+            "phonenumber": "0775778887",
+            "username": "henry",
+            "password": "my_password",
+            "isAdmin": False
+        }
+    def tearDown(self):
+        userdb_api.delete_user_by_email(self.user_data['email'])
+
+    def test_create_user_with_no_existing_user(self):
+        user_id = userdb_api.create_user(**self.user_data)
+        self.assertIs(type(user_id), int) 
+        
+    
+    def test_create_user_with_existing_user(self):
+        userdb_api.create_user(**self.user_data)
+        response = userdb_api.create_user(**self.user_data)
+        self.assertFalse(response) 
+    
+    def test_create_user_with_required_field_missing(self):
+        self.user_data.popitem()
+        userdb_api.create_user(**self.user_data)
+        self.assertRaises(Exception)
+
+   
+
+        
+        
+        
