920005e647440e4e991bfb73830d4be0df8f92a6
Geoffrey Willis <benwill2014@gmail.com>
2019-02-07 10:25:50 +0300

    Modify config file to read database url from environment

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 postgres_local_base = 'postgresql://postgres:@localhost:/'
-#heroku_base = 'postgres://pwnanpdbdxhouu:0d0fa27f64c27e6753f00c1022a6360a63f7541d4372cbd6c04e22c651936e5e@ec2-54-227-246-152.compute-1.amazonaws.com:5432/'
-#dev_database_name = 'dfluufvoinldmi'
 test_database_name = 'travis_ci_test'
 database_url = os.getenv('DATABASE_URL')
 
@@ -26,4 +24,4 @@ class ProductionConfig(BaseConfig):
     """Production configuration."""
     SECRET_KEY = 'really_secret'
     DEBUG = False
-    DATABASE_URL = 'postgresql:///example'
+    DATABASE_URL = database_url
